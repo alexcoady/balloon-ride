bd4889ef7dbdaf25193a72f89201c0cc44b3f6a9
Alex Coady <alexandercoady@me.com>
2014-02-16 13:39:29 +0000

    Added project files

---
+++ Assets/scripts/AssetDestroyer.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+
+function OnTriggerEnter2D (hit: Collider2D) {
+
+	Debug.Log("Destroying object: " + hit.tag);
+
+	Destroy (hit.gameObject);
+}
---
+++ Assets/scripts/CameraFollow.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+
+var player: Transform;
+
+function Update () {
+	
+	transform.position = new Vector3( player.position.x, player.position.y, -10 );
+}
